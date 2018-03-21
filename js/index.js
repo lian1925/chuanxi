0009e16d2819ea7a8f59f8fbc94b320afe1a01cb
lian1925 <lian1925@qq.com>
2018-03-21 16:57:54 +0800

    Site updated: 2018-03-21 16:57:51

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
 var pub_img_current_image;
 
 function backgroundImage() {
-  var pub_img_path = 'http://www.ruanyifeng.com/images_pub/';
   pub_img_path = 'http://www.liangyl.com/art_image2/';
   var pub_img_num = 55;
   var pub_img_current_no = function() {
     return Math.floor(Math.random() * pub_img_num + 1);
-    // return 121;
   };
 
   var pub_img_url = function() {
@@ -21,7 +19,7 @@ function backgroundImage() {
   index_bg.backgroundImage = 'url(' + pub_img_current_image + ')';
   // alert(pub_img_current_image)
 
-  // index_bg.transition = ' all 2s ease-out';
+  index_bg.transition = ' all 2s ease-out';
 };
 var index_bg = document.getElementById('index-bg');
 if (index_bg != null) {
