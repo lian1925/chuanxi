b3ab0bba2542def182e332f61316df18d4c543e5
lian1925 <lian1925@qq.com>
2018-04-26 09:09:50 +0800

    Site updated: 2018-04-26 09:09:21

--- js/post.js
+++ js/post.js
@@ -172,6 +172,54 @@ if(elementPlayer !== null){
     }
   });
 }
+var elementVideo = document.getElementById('video');
+function updateVideo( ) {
+  var canvas = document.getElementById( 'canvas' );
+  var ctx = canvas.getContext( '2d' );
+  // var myVideo = document.getElementById( 'video' );
+  ctx.drawImage( elementVideo, 0, 0, 640, 480 );
+}
+if(elementVideo !== null){
+
+
+  var min_w = 300; // minimum video width allowed
+  var vid_w_orig;  // original video dimensions
+  var vid_h_orig;
+
+  jQuery(function() { // runs after DOM has loaded
+
+      vid_w_orig = parseInt(jQuery('video').attr('width'));
+      vid_h_orig = parseInt(jQuery('video').attr('height'));
+      $('#debug').append("<p>DOM loaded</p>");
+
+      jQuery(window).resize(function () { resizeToCover(); });
+      jQuery(window).trigger('resize');
+  });
+
+  function resizeToCover() {
+
+      // set the video viewport to the window size
+      jQuery('#video').width(jQuery('.post').width());
+      jQuery('#video').height(jQuery('.post').width()*0.625);
+
+      // use largest scale factor of horizontal/vertical
+      var scale_h = jQuery(window).width() / vid_w_orig;
+      var scale_v = jQuery(window).height() / vid_h_orig;
+      var scale = scale_h > scale_v ? scale_h : scale_v;
+
+      // don't allow scaled width < minimum video width
+      if (scale * vid_w_orig < min_w) {scale = min_w / vid_w_orig;};
+
+      // now scale the video
+      jQuery('video').width(scale * vid_w_orig);
+      jQuery('video').height(scale * vid_h_orig);
+      // and center it by scrolling the video viewport
+      jQuery('#video').scrollLeft((jQuery('video').width() - jQuery(window).width()) / 2);
+      jQuery('#video').scrollTop((jQuery('video').height() - jQuery(window).height()) / 2);
+  };
+
+
+}
 try{
 
   　　top.location.hostname;
